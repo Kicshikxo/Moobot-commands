480e3dff612bb43d38d5929d1b1c5a368d2aead6
Kicshikxo <41877938+Kicshikxo@users.noreply.github.com>
2020-05-30 20:09:43 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const http = require('http')
+const http = require('http'),
+	  url  = require('url');
 const PORT = process.env.PORT || 3000
 const server = http.createServer(function(req, res) {
 	res.writeHeader(200, {"Content-Type": "application/json"})
-	res.write((Math.random() > 0.5) ? 'Да' : 'Нет')
+	if (url.parse(req.url).pathname == '/'){
+		
+	}
+	else if (url.parse(req.url).pathname == '/ask'){
+		res.write((Math.random() > 0.5) ? 'Да' : 'Нет')
+	}
 	return res.end()
 })
 server.listen(PORT)
