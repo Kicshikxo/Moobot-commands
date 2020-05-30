ece4c880a92c8c363950f9e266c62154c3b02015
Kicshikxo <41877938+Kicshikxo@users.noreply.github.com>
2020-05-30 19:23:42 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const http = require('http')
 const PORT = process.env.PORT || 3000
 const server = http.createServer(function(req, res) {
-	if (Math.random() > 0.5) 
-		res.writeHead(301, { "Location": "https://dl.dropbox.com/s/cfdkxvy6m1fwltt/answer-yes.json?raw=1" })
-	else
-		res.writeHead(301, { "Location": "https://dl.dropbox.com/s/7tkxb3dwglhplz3/answer-no.json?raw=1" })
+	res.writeHeader(200, {"Content-Type": "application/json"})
+	res.write((Math.random() > 0.5) ? 'Да' : 'Нет')
 	return res.end()
 })
 server.listen(PORT)
