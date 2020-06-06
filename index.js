a6942eb1e138a9e54b19a4b9b690fb4109e683fd
Kicshikxo <vip.game.game6@gmail.com>
2020-06-06 19:18:55 +0300

    update

--- index.js
+++ index.js
@@ -567,7 +567,7 @@ commands = {
 		}
 		occupiedSpace = 0
 		for (i of user.inventory) occupiedSpace += i.quantity
-		res.write(' Имя: '+user.name+', Балланс: '+user.money+'$, Рюкзак: '+occupiedSpace+'/'+user.backpackSize+', Уровень кирки: '+user.pickaxeLevel+', '+((user.swordLevel > 0) ? 'Уровень меча: '+user.swordLevel : 'Меча нет')+'.')
+		res.write(' Имя: '+user.name+', Баланс: '+user.money+'$, Рюкзак: '+occupiedSpace+'/'+user.backpackSize+', Уровень кирки: '+user.pickaxeLevel+', '+((user.swordLevel > 0) ? 'Уровень меча: '+user.swordLevel : 'Меча нет')+'.')
 	},
 	inventory: function(res, collection, user){
 		total = 0
