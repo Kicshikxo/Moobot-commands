d4c3f22bbf8202939057222016dd75fa854ac352
Kicshikxo <vip.game.game6@gmail.com>
2020-06-13 19:40:58 +0300

    update

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ const server = http.createServer(function(request, response) {
 				command = command.replace(/\+\/\+/g,'/').replace(/\/\+/g,'/').replace(/\+\//g,'/').replace(/\//g,'/')
 				command = command.replace(/\+\+\+/g,'+').replace(/\+\+/g,'+')
 // 				command = command.replace(/м/g,'-').replace(/у/g,'*').replace(/с/g,'**').replace(/д/g,'/')
-				response.write(eval(command).toString().replace('true','').replace('false',''))
+				response.write(eval(command))
 			}
 			catch (error) {
 				response.write(` Ошибка: ${error}`)
