aa4bb18258caaebfc8119609f50b54a7aa3ffeab
Kicshikxo <vip.game.game6@gmail.com>
2020-06-13 23:51:57 +0300

    update

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ const server = http.createServer(function(request, response) {
 		response.end()
 	}
 	else if (queryArguments[0] == 'eval'){
-		if (queryArguments[1] != '')
+		if (queryArguments[1])
 			try {
 				command = queryArguments[1].replace(/date/g, 'Date').replace(/math/g, 'Math').replace(/pi/g, 'PI')
 				command = command.replace(/\+\-\+/g,'-').replace(/\-\+/g,'-').replace(/\+\-/g,'-').replace(/\-/g,'-')
