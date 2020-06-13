a3b867dd4deff58fa5957fca9f805e63ec472787
Kicshikxo <vip.game.game6@gmail.com>
2020-06-13 20:10:06 +0300

    update

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const server = http.createServer(function(request, response) {
 	queryArguments.shift()
 	queryArguments = queryArguments.map(function(element){
 		if (parseInt(element) == element || parseFloat(element) == element) return element
-		return url.domainToUnicode(element.replace('@','').replace('%40','').replace(/%2F/g,'/'))
+		return url.domainToUnicode(element.replace('@','').replace('%40','').replace(/%2F/g,'слэш')).replace(/слэш/g,'/')
 	})
 	for (i = 0; i <= 5 - queryArguments.length; i++) queryArguments.push('')
 	
