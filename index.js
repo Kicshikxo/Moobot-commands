7f0179e78a3caf01d38590031aa960c85ce13422
Kicshikxo <vip.game.game6@gmail.com>
2020-06-17 03:10:07 +0300

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ String.prototype.in = function(arr){return arr.indexOf(this.toString()) != -1}
 const server = http.createServer(function(request, response) {
 	response.writeHeader(200, {"Content-Type": "application/json"})
 	if (new Date().getMonth() == 5 && new Date().getDate() < 20){
-		timeend= new Date(2020, 5, 20, 0, 0);
+		timeend= new Date(2020, 5, 19, 21, 0);
 		today = new Date();
 		today = Math.floor((timeend-today)/1000);
 		tsec=today%60; today=Math.floor(today/60); if(tsec<10)tsec='0'+tsec;
