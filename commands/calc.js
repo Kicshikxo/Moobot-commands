05d9bb55151c336f301b93d2b745db10c72ffe64
Kicshikxo <supermegavalera9000@gmail.com>
2021-11-07 10:13:19 +0300

    updated calc

--- commands/calc.js
+++ commands/calc.js
@@ -4,6 +4,9 @@ module.exports = {
     },
     handler: async (params) => {
         const { q: example } = params
+        if (!example) {
+            return "Введите пример. Например '2+2' или '123 **3 - 123456/ 2'"
+        }
 
         return JSON.stringify(await eval(example) || '')
     }
