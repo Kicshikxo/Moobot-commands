d8c6ccbf5e4d3672133966c67fe87ed8863eb729
Kicshikxo <supermegavalera9000@gmail.com>
2021-12-28 16:48:58 +0300

    added "random" param to ask command

--- commands/ask.js
+++ commands/ask.js
@@ -6,7 +6,7 @@ module.exports = {
     },
     handler: async (params) => {
         const { q: question } = params
-        const answer = sha256(question).replace(/[^\d]/g, '').split('').map(parseFloat).reduce((acc, value) => acc + value) % 2 == 0
+        const answer = (params.random == 'false' || params.random == '0') ? sha256(question).replace(/[^\d]/g, '').split('').map(parseFloat).reduce((acc, value) => acc + value) % 2 == 0 : Math.random() >= 0.5
         return `Ответ на вопрос ${question} - ${(answer) ? 'Да' : 'Нет'}`
     }
 }
