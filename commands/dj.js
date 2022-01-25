ff0d3297d38f064ef64143fe2be4925a7469c5fd
Kicshikxo <supermegavalera9000@gmail.com>
2022-01-25 12:29:54 +0300

    added count command for dj

--- commands/dj.js
+++ commands/dj.js
@@ -37,6 +37,15 @@ module.exports = {
                 return `Количество треков: ${Object.keys(response).length}. ${Object.keys(response).map(index => `${index} - ${response[index].title}; `).join('')}`
             }
         }
+        else if (['count', 'количество', 'колво', 'кол-во', 'сколько', 'число'].includes(query.toLowerCase())) {
+            const { data: response } = await axios.get(`https://streamdj.ru/api/playlist/${channelId}/c`)
+
+            if (response === false)
+                return 'Список треков пуст :('
+            else {
+                return `Количество треков: ${Object.keys(response).length}.`
+            }
+        }
         else if (['link', 'ссылка'].includes(query.toLowerCase()) && url) {
             return `Ссылка на диджея: ${url}`
         }
