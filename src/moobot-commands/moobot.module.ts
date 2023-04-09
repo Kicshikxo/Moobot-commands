a8e3fc849a6d214a1f2e5ca7a19fe7e390e71d7a
Kicshikxo <supermegavalera9000@gmail.com>
2023-04-09 22:18:22 +0300

    Change imports

--- src/moobot-commands/moobot.module.ts
+++ src/moobot-commands/moobot.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
-import { PrismaService } from 'src/prisma.service'
 import { MoobotController } from './moobot.controller'
 import { MoobotService } from './moobot.service'
+import { PrismaService } from './prisma/prisma.service'
 
 @Module({
     imports: [],
