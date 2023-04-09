55c91215a9bc842b72d5591f9d60e4749a61e374
Kicshikxo <supermegavalera9000@gmail.com>
2023-04-10 00:05:23 +0300

    Add ask and gpt endpoints

--- src/moobot-commands/moobot.controller.ts
+++ src/moobot-commands/moobot.controller.ts
@@ -1,12 +1,79 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Query } from '@nestjs/common'
+import { ApiOkResponse, ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger'
 import { MoobotService } from './moobot.service'
 
 @Controller()
+@ApiTags('Команды')
 export class MoobotController {
     constructor(private readonly moobotService: MoobotService) {}
 
-    @Get('/')
-    getHello() {
-        return this.moobotService.getHello()
+    @Get('ask')
+    @ApiOperation({ summary: 'Ответить на вопрос да или нет' })
+    @ApiOkResponse({
+        schema: {
+            type: 'string',
+            example: 'Ответ на вопрос команда крутая? - Да'
+        },
+        description: 'Ответ на вопрос'
+    })
+    @ApiQuery({
+        name: 'full-answer',
+        description:
+            "Использовать полный ответ на вопрос по шаблону: 'Ответ на вопрос ${question} - ${answer}' (по умолчанию true)",
+        type: Boolean,
+        required: false,
+        example: true
+    })
+    @ApiQuery({
+        name: 'random',
+        description: 'Отвечать на вопрос рандомно, или всегда отвечать на одинаковые вопросы одинаково (по умолчанию true)',
+        type: Boolean,
+        required: false,
+        example: false
+    })
+    @ApiQuery({
+        name: 'question',
+        description: 'Текст вопроса',
+        type: String,
+        required: true,
+        example: 'команда крутая?'
+    })
+    ask(
+        @Query('question') question: string,
+        @Query('random') random: boolean,
+        @Query('full-answer') fullAnswer: boolean
+    ): string {
+        return this.moobotService.ask({
+            question: question,
+            random: random ?? true,
+            fullAnswer: fullAnswer ?? true
+        })
+    }
+
+    @Get('gpt')
+    @ApiOperation({ summary: 'Задать вопрос ChatGPT' })
+    @ApiOkResponse({
+        schema: {
+            type: 'string',
+            example: 'Привет! Чем я могу Вам помочь?'
+        },
+        description: 'Ответ на вопрос'
+    })
+    @ApiQuery({
+        name: 'question',
+        description: 'Текст вопроса',
+        type: String,
+        required: true,
+        example: 'Привет'
+    })
+    @ApiQuery({
+        name: 'key',
+        description: 'OpenAI API Key',
+        type: String,
+        required: true,
+        example: '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'
+    })
+    async gpt(@Query('question') question: string, @Query('key') key: string): Promise<string | undefined> {
+        return await this.moobotService.gpt({ question, key })
     }
 }
--- src/moobot-commands/moobot.service.ts
+++ src/moobot-commands/moobot.service.ts
@@ -1,11 +1,38 @@
 import { Injectable } from '@nestjs/common'
+import { createHash } from 'crypto'
+import { Configuration, OpenAIApi } from 'openai'
 import { PrismaService } from './prisma/prisma.service'
 
 @Injectable()
 export class MoobotService {
     constructor(private readonly prismaService: PrismaService) {}
 
-    async getHello() {
-        return this.prismaService.test.findMany()
+    ask(options: { question: string; random: boolean; fullAnswer: boolean }): string {
+        const answer =
+            options.random == true
+                ? Math.random() >= 0.5
+                : createHash('sha256')
+                      .update(options.question.toLowerCase())
+                      .digest('hex')
+                      .replace(/[^\d]/g, '')
+                      .split('')
+                      .map(parseFloat)
+                      .reduce((acc, value) => acc + value) %
+                      2 ==
+                  0
+
+        return options.fullAnswer
+            ? `Ответ на вопрос ${options.question} - ${answer ? 'Да' : 'Нет'}`
+            : `${answer ? 'Да' : 'Нет'}`
+    }
+
+    async gpt(options: { question: string; key: string }): Promise<string | undefined> {
+        const openai = new OpenAIApi(new Configuration({ apiKey: options.key }))
+        const response = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            temperature: 0.3,
+            messages: [{ role: 'user', content: options.question }]
+        })
+        return response.data.choices.at(0)?.message?.content
     }
 }
